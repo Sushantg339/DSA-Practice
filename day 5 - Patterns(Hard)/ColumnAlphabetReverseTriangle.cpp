362f42bf788022a9f79985c890f9983f448c5836
Sushant Gupta <sushantg339@gmail.com>
2025-08-08 14:24:56 +0530

    learned some more patterns printing concepts- medium and hard

---
+++ day 5 - Patterns(Hard)/ColumnAlphabetReverseTriangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "enter a number : ";
+    cin >> n;
+
+    for(int i=0 ; i<n ; i++){
+        for(int sp = 0 ; sp<n-i-1 ; sp++){
+            cout << "  ";
+        }
+        for(int j=0 ; j<=i ; j++){
+            cout << char(j+65) << " ";
+        }
+        cout << endl;
+    }
+}
